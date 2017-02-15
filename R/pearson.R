0f57abed14dd78e130c9b43d0b28461c225b0f5b
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2017-02-15 11:58:19 +0000

    Fix for dimensions when imputing sampling times after deaths

--- R/pearson.R
+++ R/pearson.R
@@ -174,7 +174,7 @@ pearson.msm <- function(x, transitions=NULL, timegroups=3, intervalgroups=3, cov
             centime <- md$maxtimes[deathindex[i]] # - md$time[deathindex[i]] + mintime
             tg <- md$timegroup[deathindex[i]]
             ## returns list of times, whether would have ended in censoring, and time category.
-            st <- sampletimes(mintime, centime, empiricaldist[,tg,empiricaldist["time",tg,]>0], N, tg, intervalq)
+            st <- sampletimes(mintime, centime, empiricaldist[,tg,empiricaldist["time",tg,]>0,drop=FALSE], N, tg, intervalq)
             for (j in c("times","cens","intervalgroup"))
                 imputation[i,,j] <- st[,j]
         }
@@ -573,6 +573,7 @@ empiricaldists  <-  function(timeinterval, state, obgroup, obgroups, ndstates) {
 
 sampletimes <- function(mintime, centime, dist, N, obgroup, intervalq) {
     ## dist takes the overall distribution from the relevant obgroup, but since we supply obgroup it might be better to just supply the whole thing
+    dist <- array(dist, dim=dim(dist)[c(1,3)], dimnames=dimnames(dist)[c(1,3)])
     dist <- dist[,dist[1,]>mintime,drop=FALSE] # Remove times before the minimum possible time.
     if (length(dist) > 0) {
         dist[c("surv","pdeath"),] <- dist[c("surv","pdeath"),] / dist["surv",1] # Normalise
